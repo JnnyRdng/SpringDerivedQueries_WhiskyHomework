f6deee21112cf2d09d4e1cdef9037fc5e5473e13
Jonny Reading <jonnyreading@gmail.com>
2020-10-07 21:18:04 +0100

    add request param to index route to get by year

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +16,10 @@ public class WhiskyController {
     private WhiskyRepository whiskyRepository;
 
     @GetMapping(value = "/whiskies")
-    public ResponseEntity getAllWhiskies() {
+    public ResponseEntity getAllWhiskies(@RequestParam(name = "year", required = false) Integer year) {
+        if (year != null) {
+            return new ResponseEntity(whiskyRepository.findWhiskyByYear(year), HttpStatus.OK);
+        }
         return new ResponseEntity(whiskyRepository.findAll(), HttpStatus.OK);
     }
 
