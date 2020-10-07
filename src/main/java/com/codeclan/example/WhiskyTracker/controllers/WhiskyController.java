d6868a593d15264279e7d6480fa80766d4214c68
Jonny Reading <jonnyreading@gmail.com>
2020-10-07 21:13:02 +0100

    get whiskies by year tests

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,9 +1,22 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WhiskyController {
+
+    @Autowired
+    private WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity getAllWhiskies() {
+        return new ResponseEntity(whiskyRepository.findAll(), HttpStatus.OK);
+    }
 
 }
--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,34 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	private WhiskyRepository whiskyRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void canGetWhiskyByYear() {
+		List<Whisky> found = whiskyRepository.findWhiskyByYear(2018);
+		assertEquals(6, found.size());
+		found = whiskyRepository.findWhiskyByYear(2014);
+		assertEquals(4, found.size());
 	}
 
 }
